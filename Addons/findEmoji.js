981dde037afef716a5a8d323dafd6e84e46eb996
Rafski <page+github@kalinowski.app>
2023-04-28 21:00:30 +0200

    28.4.23

--- Addons/findEmoji.js
+++ Addons/findEmoji.js
@@ -1,11 +1,12 @@
 /**
- * Finds an emoji object with a given name on a specific guild object.
- * @param {object} client - The client object.
- * @param {string} emojiName - The name of the emoji to find on TEA server.
- * @returns {string} The string representation of the emoji or '🐛' if not found.
+ * Finds an emoji by name on a specific guild and returns its string representation.
+ *
+ * @param {import("discord.js").Client} client - The Discord client instance.
+ * @param {string} emojiName - The name of the emoji to find.
+ * @returns {string} The string representation of the emoji, or '🐛' if the emoji is not found.
  */
 function findEmoji(client, emojiName) {
-    const guild = client.guilds.cache.get(process.env.TEA_GUILD_ID);
+    const guild = client.guilds.cache.get(process.env.TEA_GUILD_ID); // get the guild object from the cache
     const emoji = guild.emojis.cache.find(e => e.name === emojiName); // find the emoji by name
 
     if (emoji) {
