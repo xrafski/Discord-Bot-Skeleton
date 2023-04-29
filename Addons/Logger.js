cdbbb165ae7e691eedca9dae77c7893f54e2314d
Rafski <page+github@kalinowski.app>
2023-04-29 11:39:33 +0200

    29.4.23

--- Addons/Logger.js
+++ Addons/Logger.js
@@ -4,11 +4,11 @@ const moment = require('moment');
 const format = 'DD/MM/YYYY - hh:mm:ss A [UTC]';
 
 const log = {
-    log: function (message) { console.log(`[${moment().utc().format(format)}] [LOG]`, message); },
-    info: function (message) { console.info(`[${moment().utc().format(format)}] [INFO]`, message); },
-    debug: process.env.BOT_DEBUG === 'true' ? function (message) { console.debug(`[${moment().utc().format(format)}] ⭕ [DEBUG]`, message); } : () => { },
-    warn: function (message, error) { console.warn(`[${moment().utc().format(format)}] [WARN]`, message, error); },
-    bug: function (message, error) { console.error(`[${moment().utc().format(format)}] [🐛 BUG]`, message, error); },
+    log: function (...args) { console.log(`[${moment().utc().format(format)}] [LOG]`, ...args); },
+    info: function (...args) { console.info(`[${moment().utc().format(format)}] [INFO]`, ...args); },
+    debug: process.env.BOT_DEBUG === 'true' ? function (...args) { console.debug(`[${moment().utc().format(format)}] ⭕ [DEBUG]`, ...args); } : () => { },
+    warn: function (...args) { console.warn(`[${moment().utc().format(format)}] [WARN]`, ...args); },
+    bug: function (...args) { console.error(`[${moment().utc().format(format)}] [🐛 BUG]`, ...args); },
 };
 
 module.exports = log;
